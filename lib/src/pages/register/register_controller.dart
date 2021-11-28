d3e2c5651be223e88a36ada9248ceb471470e93e
Josselyn-Troya <josselyn.troya@epn.edu.ec>
2021-11-28 11:33:23 -0500

    validations

--- lib/src/pages/register/register_controller.dart
+++ lib/src/pages/register/register_controller.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:lospescaditosdmary/src/models/response_api.dart';
 import 'package:lospescaditosdmary/src/models/user.dart';
 import 'package:lospescaditosdmary/src/provider/users_provider.dart';
+import 'package:lospescaditosdmary/src/utils/my_validations.dart';
 
 class RegisterController {
   BuildContext context;
@@ -27,6 +28,22 @@ class RegisterController {
     String password = passwordController.text.trim();
     String confirmPassword = confirmPasswordController.text.trim();
 
+    if(email.isEmpty || name.isEmpty || lastname.isEmpty || phone.isEmpty || password.isEmpty || confirmPassword.isEmpty){
+      MyValidations.show(context, 'Debes completar todos los campos');
+      
+      return;
+    }
+
+    if(confirmPassword != password){
+      MyValidations.show(context, 'Las contraseñas no son iguales');
+      return;
+    }
+
+    if(password.length <6 ){
+      MyValidations.show(context, 'La contraseña debe tener al menos 6 caracteres');
+      return;
+    }
+
     User user = new User(
       email: email,
       name: name,
@@ -36,6 +53,9 @@ class RegisterController {
     );
 
     ResponseApi responseApi = await usersProvider.create(user);
+
+    MyValidations.show(context, responseApi.message);
+
     print('respuesta: ${responseApi.toJson()}');
 
     print(email);
